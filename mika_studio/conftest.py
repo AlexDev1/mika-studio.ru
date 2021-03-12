60f79e4be55da90477ab0725216c4fea7a7a0df8
Alex-iMac <alexbog80@gmail.com>
2021-03-12 14:25:42 +0500

    Init repo

---
+++ mika_studio/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from mika_studio.users.models import User
+from mika_studio.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
